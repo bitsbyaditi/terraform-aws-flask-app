a81f7f4280b34339911d05e3592a7971de7714f2
EC2 Default User <ec2-user@ip-10-0-1-190.ap-south-1.compute.internal>
2026-06-10 11:33:16 +0000

    Added professional dashboard UI with templates and updated Flask routing

--- app/app.py
+++ app/app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>AWS Terraform Project</h1>
-    <p>Dockerized Flask Application Running Successfully!</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
